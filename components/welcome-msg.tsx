2c845df596e014b552cf9f25a8b75ee94516ce3a
Caubi Neto <caubi@yantec.com.br>
2024-05-18 23:17:32 -0300

    :construction: Navbar

---
+++ components/welcome-msg.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+
+export const WelcomeMsg = () => {
+    const { user, isLoaded } = useUser();
+
+    return (
+        <div className="space-y-2 mb-4">
+            <h2 className="text-2xl lg:text-4xl text-white font-medium"> 
+                Bem vindo(a) de volta{isLoaded ? ", " : " "}{user?.firstName} 👋
+            </h2>
+            <p className="text-sm lg:text-base text-[#89B6FD]"> 
+            Este é o seu relatório de visão geral financeira
+            </p>
+        </div>
+    );
+};
