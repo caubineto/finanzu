65c13a2bc861281c5162d33a042507df6c4d935b
Caubi Neto <caubineto@outlook.com>
2024-06-04 23:25:43 -0300

    📊: Dashboard charts

--- components/data-card.tsx
+++ components/data-card.tsx
@@ -90,7 +90,7 @@ export const DataCard = ({
                     percentageChange > 0 && "text-emerald-500",
                     percentageChange < 0 && "text-rose-500",
                 )}>
-                    {formatPercentage(percentageChange)} do ultimo período
+                    {formatPercentage(percentageChange, { addPrefix: true })} do ultimo período
                 </p>
             </CardContent>
         </Card>
